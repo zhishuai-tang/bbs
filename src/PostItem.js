827bef4f7708ee7da870a8f9eaed40163828658b
zhishuai-tang <zjwytzsh@qq.com>
2018-10-16 23:50:10 +0800

    状态分离，把state及点赞处理逻辑移到PostList

--- src/PostItem.js
+++ src/PostItem.js
@@ -1,18 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class PostItem extends Component {
-    render() {
-        const { title, author, date } = this.props;
+ function PostItem(props) {
 
-        return (
-            <li>
-                <div>{ title }</div>
-                <div>创建人：<span>{ author }</span></div>
-                <div>创建时间：<span>{ date }</span></div>
-            </li>
-        );
+    const handleClick = () => {
+        props.onVote(props.post.id);
     }
 
+    
+    const { post } = props;
+    return (
+        <li>
+            <div>
+                { post.title }
+            </div>
+            <div>
+                创建人：<span>{ post.author }</span>
+            </div>
+            <div>
+                创建时间：<span>{ post.date }</span>
+            </div>
+            <div>
+                <button onClick={ handleClick }>
+                    点赞
+                </button>
+                &nbsp;
+                <span>{ post.vote }</span>
+            </div>
+        </li>
+    );
 }
 
 export default PostItem;
--- src/PostList.js
+++ src/PostList.js
@@ -1,23 +1,64 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import PostItem from './PostItem';
 
-const data = [
-    { title: "大家一起来讨论React吧", author: "张三", date: "2017- 09- 01 10: 00" }, 
-    { title: "前端框架，你最爱哪一个", author: "李四", date: "2017- 09- 01 12: 00" }, 
-    { title: "Web App的时代已经到来", author: "王五", date: "2017- 09- 01 14: 00" }
-];
+// const data = [
+//     { title: "大家一起来讨论React吧", author: "张三", date: "2017-09-01 10:00" }, 
+//     { title: "前端框架，你最爱哪一个", author: "李四", date: "2017-09-01 12:00" }, 
+//     { title: "Web App的时代已经到来", author: "王五", date: "2017-09-01 14:00" }
+// ];
 
 class PostList extends Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            posts: []
+        };
+        this.timer = null; // 定时器
+    }
+
+    componentDidMount() {
+        // 用setTimeout模拟异步从服务器端获取数据
+        this.timer = setTimeout(() => {
+            this.setState({
+                posts: [
+                    { id: 1, title: "大家一起来讨论React吧", author: "张三", date: "2017-09-01 10:00", vote: 0 }, 
+                    { id: 2, title: "前端框架，你最爱哪一个", author: "李四", date: "2017-09-01 12:00", vote: 0 }, 
+                    { id: 3, title: "Web App的时代已经到来", author: "王五", date: "2017-09-01 14:00", vote: 0 } 
+                ]
+            });
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+        if (this.timer) {
+            clearTimeout(this.timer); // 清除定时器
+        }
+    }
+
+    handleVote = (id) => {
+        // 根据帖子id进行过滤，找到待修改vote属性的帖子，返回新的posts对象
+        const posts = this.state.posts.map(item => {
+            const newItem = item.id === id ? {...item, vote: ++item.vote} : item;
+            return newItem;
+        });
+
+        // 使用新的posts对象设置state
+        this.setState({
+            posts: posts
+        });
+    }
+
     render() {
         return (
             <div>
                 帖子列表：
                 <ul>
-                    {data.map(item =>
-                        <PostItem
-                            title = { item.title }
-                            author = { item.author }
-                            date = { item.date }
+                    {this.state.posts.map(item =>
+                        <PostItem 
+                            post = { item }
+                            onVote = { this.handleVote }
                         />
                     )}
                 </ul>
@@ -26,4 +67,4 @@ class PostList extends Component {
     }
 }
 
-export default PostList
+export default PostList;
